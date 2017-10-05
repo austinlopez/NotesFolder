515691d00d5931d2767725c7f707b896a88e34f4
Austin Lopez <lopeza1857@aguafria.org>
2017-10-05 08:41:59 -0700

    MidClass

--- Better Animation/betterAnimation.js
+++ Better Animation/betterAnimation.js
@@ -21,6 +21,15 @@ var canvas = document.getElementById("theCanvas");
 //ctx holds the drawing context
 var ctx = canvas.getContext("2d");
 
+var hexColor = ["1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f"];
+function randomColor(){
+  actualColor = "#000000";
+
+  actualColor = "#"+hexColor[Math.floor(Math.random()*hexColor.length)]+hexColor[Math.floor(Math.random()*hexColor.length)]+hexColor[Math.floor(Math.random()*hexColor.length)]+hexColor[Math.floor(Math.random()*hexColor.length)]+hexColor[Math.floor(Math.random()*hexColor.length)]+hexColor[Math.floor(Math.random()*hexColor.length)];
+  return actualColor;
+}
+
+
 //classes
 class Ball {
   //member variables
@@ -55,12 +64,14 @@ class Ball {
     ctx.arc(this.x,this.y,this.radius,0,2*Math.PI);
     ctx.fill();
   }
+  clear(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+  }
   move(){
     //Description: this function will set the movement
     //direction by changing the x and y
     //input: none
     //output: increment or decrement the correct x/y variable
-    ctx.clearRect(0,0,canvas.width,canvas.height);
     switch (this.direction) {
       case movementDirection.RIGHT:
         this.x += this.speed;
@@ -93,19 +104,41 @@ class Ball {
     if (this.y < (0-this.radius)){this.y = (canvas.height+this.radius)}
   }
 }
-
+var count = 0;
 //functions
 function animate(){ //loops for the animation
+  b.clear();
   b.move();
   b.checkLoop();
+  for (var i = 0; i < ballList.length; i++) {
+    ballList[i].move();
+    ballList[i].checkLoop();
+  }
+  count++;
+  if (ballList.length < 1000) {
+    if(count % 1 == 0){
+      //ballList.push(new Ball(30,0,10,"red",1,movementDirection.DOWN));
+      ballList.push(new Ball(Math.floor(Math.random()*800),Math.floor(Math.random()*400),Math.floor(Math.random()*50),randomColor(),Math.floor(Math.random()*20+1),movementDirection.DOWN));
+    }
+  }
   document.getElementById("x").innerHTML = "x:" + b.x;
   document.getElementById("y").innerHTML = "y:" + b.y;
   document.getElementById("radius").innerHTML = "radius:" + b.radius;
   document.getElementById("speed").innerHTML = "Speed:" + b.speed;
+  document.getElementById("numberOfShapes").innerHTML = "    Number of Shapes:" + ballList.length;
 }
 
 var b = new Ball(50,50,70,"red",1,movementDirection.RIGHT)
+var ballList = [new Ball(50,50,10,"blue",1,movementDirection.DOWN),
+new Ball(Math.floor(Math.random()*400),Math.floor(Math.random()*400),Math.floor(Math.random()*50),"red",Math.floor(Math.random()*20),movementDirection.DOWN)];
+
+
 window.setInterval(animate,10);
+/*var ball1 = new Ball(50,100,50,"blue",10,movementDirection.RIGHT)
+var ball2 = new Ball(55,200,80,"green",15,movementDirection.RIGHT)
+var ball3 = new Ball(60,300,100,"orange",20,movementDirection.RIGHT)
+var ball4 = new Ball(70,400,120,"grey",25,movementDirection.RIGHT)
+*/
 
 window.addEventListener("keydown",eventHandler);
 function eventHandler(event){
@@ -113,15 +146,27 @@ function eventHandler(event){
   switch(keyCode){
     case 37: //left arrow keyCode
     b.setDirection(movementDirection.LEFT);
+    for (var i = 0; i < ballList.length; i++) {
+      ballList[i].setDirection(movementDirection.LEFT);
+    }
     break;
     case 38: //up arrow keyCode
     b.setDirection(movementDirection.UP);
+    for (var i = 0; i < ballList.length; i++) {
+      ballList[i].setDirection(movementDirection.UP);
+    }
     break;
     case 39: //right arrow keyCode
     b.setDirection(movementDirection.RIGHT);
+    for (var i = 0; i < ballList.length; i++) {
+      ballList[i].setDirection(movementDirection.RIGHT);
+    }
     break;
     case 40: //down arrow keyCode
     b.setDirection(movementDirection.DOWN);
+    for (var i = 0; i < ballList.length; i++) {
+      ballList[i].setDirection(movementDirection.DOWN);
+    }
     break;
     case 65: //a
     if (b.speed > 0){
